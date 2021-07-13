6eebc0541e042e4c2355f4b20448471c7cc42073
Ho Cheung <hoc@genebygene.com>
2021-07-13 13:37:35 -0500

    Add docker instructions to build eagle

--- eagle-docker/eagle/build-eagle.sh
+++ eagle-docker/eagle/build-eagle.sh
@@ -10,4 +10,4 @@ DEST_MOUNT_OPT="--mount type=bind,source=$DEST_MOUNT_DIR,target=/tmp/eagle/bin"
 docker run -i $SRC_MOUNT_OPT $DEST_MOUNT_OPT --user $(id -u):$(id -g) --entrypoint 'bash' gxg-pipeline/eagle -c \
 'source scl_source enable devtoolset-9 &&\
 cmake3 -DBOOST_ROOT=/opt/boost/1.74.0 -DHTSLIB_SEARCH_DIRS=/opt/htslib/1.12 ../src &&\
- make -j$(nproc)'
+ make -j$(nproc) && make package'
